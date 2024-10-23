71a30afc562e9bc12fbdcd9cac8e3b33ee88f9ae
Akshay Khot <akshaykhot743@gmail.com>
2024-10-23 09:10:20 +0530

    Export users list functionality implemented

--- src/components/UserListingTable/SearchExportComp/index.tsx
+++ src/components/UserListingTable/SearchExportComp/index.tsx
@@ -1,8 +1,29 @@
+import useModifiedUsers from "@/hooks/useModifiedUsers";
 import useUserList from "@/hooks/useUserList";
 import { RxReset } from "react-icons/rx";
 
 const SearchExportComp = () => {
   const { sortFilterState, sortFilterDispatch } = useUserList();
+  const modifiedUsers = useModifiedUsers(sortFilterState);
+  
+  const handleDownloadUsersData = () => {
+    let str = "date,name,email,source,status\n";
+    modifiedUsers?.forEach((el) => {
+      const row = `${el.date.split(",")[0].trim()},${el.name},${el.email},${el.source},${el.status}\n`;
+      str = str + row;
+    });
+
+    const element = document.createElement("a");
+    const sampleFile = new Blob([`${str}`], {
+      type: "text/csv",
+    });
+
+    element.href = URL.createObjectURL(sampleFile);
+    element.download = "users list.csv";
+    document.body.appendChild(element);
+    element.click();
+    document.body.removeChild(element);
+  };
 
   return (
     <div className="flex items-center justify-between">
@@ -51,6 +72,7 @@ const SearchExportComp = () => {
 
       <button
         type="button"
+        onClick={handleDownloadUsersData}
         className="export-btn bg-gradient-to-r from-purple-100 to-purple-200 text-white font-medium text-sm px-8 py-2 rounded-3xl"
       >
         Export
